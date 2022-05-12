70f0ba3d16874e1024e5a96992f510f472b4b839
Eze Chibuzor <buzoreze12@hotmail.com>
2022-05-12 15:48:50 +0100

    le commit numero deux

--- jsCopy.js
+++ jsCopy.js
@@ -2,21 +2,21 @@ function getHistory(){
 	return document.getElementById("history-value").innerText;
 }
 function printHistory(num){
-	document.getElementById("history-value").innerText=num;
+	document.getElementById("history-value").innerText = num;
 }
 function getOutput(){
 	return document.getElementById("output-value").innerText;
 }
 function printOutput(num){
-	if(num==""){
-		document.getElementById("output-value").innerText=num;
+	if(num == ""){
+		document.getElementById("output-value").innerText = num;
 	}
 	else{
-		document.getElementById("output-value").innerText=getFormattedNumber(num);
+		document.getElementById("output-value").innerText = getFormattedNumber(num);
 	}	
 }
 function getFormattedNumber(num){
-	if(num=="-"){
+	if(num == "-"){
 		return "";
 	}
 	var n = Number(num);
@@ -27,32 +27,32 @@ function reverseNumberFormat(num){
 	return Number(num.replace(/,/g,''));
 }
 var operator = document.getElementsByClassName("operator");
-for(var i =0;i<operator.length;i++){
+for(var i=0; i<operator.length; i++){
 	operator[i].addEventListener('click',function(){
-		if(this.id=="clear"){
+		if(this.id == "clear"){
 			printHistory("");
 			printOutput("");
 		}
-		else if(this.id=="backspace"){
-			var output=reverseNumberFormat(getOutput()).toString();
+		else if(this.id == "backspace"){
+			var output = reverseNumberFormat(getOutput()).toString();
 			if(output){//if output has a value
-				output= output.substr(0,output.length-1);
+				output = output.substr(0, output.length-1);
 				printOutput(output);
 			}
 		}
 		else{
-			var output=getOutput();
-			var history=getHistory();
-			if(output==""&&history!=""){
-				if(isNaN(history[history.length-1])){
-					history= history.substr(0,history.length-1);
+			var output = getOutput();
+			var history = getHistory();
+			if(output == ""&&history != ""){
+				if(isNaN(history[history.length - 1])){
+					history = history.substr(0, history.length - 1);
 				}
 			}
-			if(output!="" || history!=""){
-				output= output==""?output:reverseNumberFormat(output);
-				history=history+output;
-				if(this.id=="="){
-					var result=eval(history);
+			if(output != "" || history != ""){
+				output = output=="" ? output : reverseNumberFormat(output);
+				history = history+output;
+				if(this.id == "="){
+					var result = eval(history);
 					printOutput(result);
 					printHistory("");
 				}
